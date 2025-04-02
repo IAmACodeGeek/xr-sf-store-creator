7164b0f838173a6c8316174fc55d6892e55d25ab
IAmCodeGeek <copewithcode@gmail.com>
2025-04-02 18:44:29 +0530

    Changed big room model

--- src/data/environment/environmentModel/BigRoom.jsx
+++ src/data/environment/environmentModel/BigRoom.jsx
@@ -2,31 +2,40 @@ import { useMemo } from 'react';
 import { useGLTFWithKTX2 } from "../../../world/useGTLFwithKTX";
 
 const BigRoomModel = () => {
-  const { nodes, materials } = useGLTFWithKTX2('/models/BigRoomH.glb');
+  const { nodes, materials } = useGLTFWithKTX2('/models/BigRoom.glb');
   
   const memoizedNodes = useMemo(() => nodes, [nodes]);
   const memoizedMaterials = useMemo(() => materials, [materials]);
 
   return (
-    <group position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]} scale={[0.9, 0.9, 0.9]} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={memoizedNodes.Mesh.geometry}
-        material={memoizedNodes.Mesh.material}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={memoizedNodes.Mesh_1.geometry}
-        material={memoizedMaterials.room_m1_light}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={memoizedNodes.Mesh_2.geometry}
-        material={memoizedMaterials.model_1}
-      />
+    <group>
+
+      <group position={[6, 0, 0]} rotation={[Math.PI / 2, 0, 0]} scale={[0.9, 0.9, 0.9]} dispose={null}>
+        <mesh
+            castShadow
+            receiveShadow
+            geometry={memoizedNodes.Mesh.geometry}
+            material={materials.model_1}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={memoizedNodes.Mesh_1.geometry}
+            material={memoizedMaterials.phong3}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={memoizedNodes.Mesh_2.geometry}
+            material={memoizedMaterials.room_m1_light}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={memoizedNodes.Mesh_3.geometry}
+            material={memoizedMaterials.phong2}
+          />
+      </group>
     </group>
   );
 };
