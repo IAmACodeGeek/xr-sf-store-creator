4d7ee5b5beb751b2ad7f8fbb18921bf0f36a623f
IAmCodeGeek <copewithcode@gmail.com>
2025-04-03 20:04:06 +0530

    Lighting fixes in singleroom

--- src/data/environment/environmentLighting/SingleRoom.jsx
+++ src/data/environment/environmentLighting/SingleRoom.jsx
@@ -1,7 +1,18 @@
 const SingleRoomLighting = () => {
+  const shadowOffset = 50;
   return (
     <>
-      <ambientLight color={0xffffff} intensity={1}></ambientLight>
+      <ambientLight intensity={5} />
+      <directionalLight
+        castShadow
+        intensity={2.5}
+        shadow-mapSize={4096}
+        shadow-camera-top={shadowOffset}
+        shadow-camera-bottom={-shadowOffset}
+        shadow-camera-left={shadowOffset}
+        shadow-camera-right={-shadowOffset}
+        position={[100, 100, 0]}
+      />
     </>
   );
 };
