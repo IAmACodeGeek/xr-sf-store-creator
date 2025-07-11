42bdf3d95dc7387382cb458b971e4c0d605ab5be
sathish-strategyfox <sathish.strategyfox@gmail.com>
2025-07-11 13:21:57 +0530

    feat: add Glowbar environment with models, lighting, and placeholder data to environment data fix : removed preloaded for the environment

--- src/data/environment/EnvironmentData.ts
+++ src/data/environment/EnvironmentData.ts
@@ -10,6 +10,7 @@ import ShelfscapePlaceHolderData from "./placeHolderData/Shelfscape";
 import KidsStorePlaceHolderData from "./placeHolderData/KidsStore";
 import ArcadeZonePlaceHolderData from "./placeHolderData/ArcadeZone";
 import VarsityVaultPlaceHolderData from "./placeHolderData/VarsityVault";
+import GlowBarPlaceHolderData from "./placeHolderData/Glowbar";
 
 interface EnvironmentData {
   [environment_name: string]: {
@@ -475,6 +476,46 @@ const environmentData: EnvironmentData = {
         rotation: [-90, 0, 0],
         scale: 5,
         dark: true,
+      },
+    ],
+  },
+  GLOWBAR: {
+    playerSpeed: 10,
+    playerHeight: 3,
+    placeHolderData: GlowBarPlaceHolderData,
+    initialGSAP: {
+      start: {
+        position: [0, 6, 0],
+        rotation: [0, 135, 0],
+        duration: 0,
+      },
+      update: [
+        {
+          position: [15.373, 6, -7.952],
+          rotation: [0, 135, 0],
+          duration: 2,
+        },
+      ],
+    },
+    televisions: [
+      {
+        position: [18.6, 6, 0],
+        rotation: [0, 180, 0],
+        scale: 0.5,
+      },
+    ],
+    brandPosters: [
+      {
+        position: [-18.6, 6, -3.5],
+        rotation: [0, 90, 0],
+        scale: 4,
+      },
+    ],
+    compasses: [
+      {
+        position: [-0.5, 0.1, 0],
+        rotation: [-90, 0, 0],
+        scale: 4,
       },
     ],
   },
